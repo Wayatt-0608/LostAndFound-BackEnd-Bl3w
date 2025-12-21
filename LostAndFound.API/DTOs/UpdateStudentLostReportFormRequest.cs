7b1cc9a1cddfe390164fb2eb20673b740cbc3863
nhan0ngu <nhannhu03@gmail.com>
2025-12-21 11:36:45 +0700

    Add API form DTOs for report image upload

---
+++ LostAndFound.API/DTOs/UpdateStudentLostReportFormRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LostAndFound.API.DTOs;
+
+public class UpdateStudentLostReportFormRequest
+{
+    public int? CategoryId { get; set; }
+    public string? Description { get; set; }
+    public DateTime? LostDate { get; set; }
+    public string? LostLocation { get; set; }
+    public IFormFile? Image { get; set; }
+}
+
