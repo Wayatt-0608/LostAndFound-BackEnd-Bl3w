8c11a723decfee2eb91ccb85b1316a9cbe7ac666
Wayatt-0608 <phanquoclam682004@gmail.com>
2025-12-21 19:48:18 +0700

    add interface and service

---
+++ LostAndFound.Application/Interfaces/FoundItems/IStaffFoundItemService.cs
@@ -0,0 +1,13 @@
+using LostAndFound.Application.DTOs.FoundItems;
+
+namespace LostAndFound.Application.Interfaces.FoundItems;
+
+public interface IStaffFoundItemService
+{
+    Task<StaffFoundItemResponse> CreateAsync(int createdBy, CreateStaffFoundItemRequest request);
+    Task<IEnumerable<StaffFoundItemResponse>> GetAllAsync(int? campusId = null, string? status = null, int? categoryId = null);
+    Task<StaffFoundItemResponse?> GetByIdAsync(int id);
+    Task<StaffFoundItemResponse?> UpdateAsync(int id, UpdateStaffFoundItemRequest request);
+    Task<StaffFoundItemResponse?> UpdateStatusAsync(int id, string status);
+}
+
